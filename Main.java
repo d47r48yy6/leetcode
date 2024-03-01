ee3b2813c71a54a7d06ecf7c2136846892b22b55
D47R48 <ayushyadav212121@gmail.com>
2024-03-01 18:07:40 +0530

    pattern 1

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of the Pattern");
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
